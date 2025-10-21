197a6c4a7b5c827e38266abbee2dce4464e2c0e3
jose enrique garcia <jose.garcia.2010@gmail.com>
2025-10-21 14:00:44 -0400

    ecmaScript

---
+++ src/Gretting.js
@@ -0,0 +1,15 @@
+export function Saludo() {
+  return <div>
+    <h1>Componente React</h1>
+    <p>lorem ipsum </p>
+  </div>
+}
+
+export function UserCard(){
+    return (
+        <div>
+            <h1>User Card</h1>
+        </div>
+    )
+    
+}
